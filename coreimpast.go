30c4954f869ada514867f07b2591b3c937ab9734
metaleap <metaleap@mailbox.org>
2017-09-27 18:41:36 +0200

    [autocommit] «Double!»

--- coreimpast.go
+++ coreimpast.go
@@ -1,14 +1,22 @@
 package main
 
 type CoreImp struct {
-	BuiltWith string              `json:"builtWith,omitempty"`
-	Imports   []string            `json:"imports,omitempty"`
-	Exports   []string            `json:"exports,omitempty"`
-	Foreign   []string            `json:"foreign,omitempty"`
-	Ast       []CoreImpRawAstItem `json:"ast,omitempty"`
+	BuiltWith string       `json:"builtWith,omitempty"`
+	Imports   []string     `json:"imports,omitempty"`
+	Exports   []string     `json:"exports,omitempty"`
+	Foreign   []string     `json:"foreign,omitempty"`
+	Body      []CoreImpAst `json:"body,omitempty"`
 }
 
-type CoreImpRawAstItem struct {
-	Tag      string        `json:"ast"`
-	Contents []interface{} `json:"contents,omitempty"`
+type CoreImpAst struct {
+	SourceSpan     *CoreImpSourceSpan `json:"sourceSpan,omitempty"`
+	_StringLiteral string
+	Tag            string        `json:"ast"`
+	Contents       []interface{} `json:"contents,omitempty"`
+}
+
+type CoreImpSourceSpan struct {
+	Name  string `json:"name,omitempty"`
+	Start []int  `json:"start,omitempty"`
+	End   []int  `json:"end,omitempty"`
 }
