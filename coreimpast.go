ec39a5a484d67ebb317320f471f406c2f3299b95
metaleap <metaleap@mailbox.org>
2017-09-27 17:13:27 +0200

    [autocommit] «A day without sunshine is like night.»

--- coreimpast.go
+++ coreimpast.go
@@ -1,14 +1,14 @@
 package main
 
 type CoreImp struct {
-	BuiltWith string           `json:"builtWith,omitempty"`
-	Imports   []string         `json:"imports,omitempty"`
-	Exports   []string         `json:"exports,omitempty"`
-	Foreign   []string         `json:"foreign,omitempty"`
-	Ast       []CoreImpAstItem `json:"ast,omitempty"`
+	BuiltWith string              `json:"builtWith,omitempty"`
+	Imports   []string            `json:"imports,omitempty"`
+	Exports   []string            `json:"exports,omitempty"`
+	Foreign   []string            `json:"foreign,omitempty"`
+	Ast       []CoreImpRawAstItem `json:"ast,omitempty"`
 }
 
-type CoreImpAstItem struct {
+type CoreImpRawAstItem struct {
 	Tag      string        `json:"ast"`
 	Contents []interface{} `json:"contents,omitempty"`
 }
